3967ce76fdb894a0d4fee7452b5539cbe406c6a8
Hassan Alsayhah <hassan.alsayhah@memryx.com>
2026-02-08 17:31:26 +0300

    add class aware nms to yolo7_det

--- src/yolo7_detect.cpp
+++ src/yolo7_detect.cpp
@@ -195,7 +195,8 @@ void Yolo7Detect::postprocess(const std::vector<float*>& outputs, Result& result
         }
     }
     // apply NMS
-    std::vector<int> keep_indices = MX::Prepost::Util::nms(all_boxes, cfg_.iou);
+    std::vector<int> keep_indices =
+            MX::Prepost::Util::nms(all_boxes, cfg_.iou, cfg_.class_agnostic);
 
     // early exit
     int num_keep = static_cast<int>(keep_indices.size());
