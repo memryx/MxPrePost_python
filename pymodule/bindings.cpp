f156b2190f3539e20e884fabcd389532abbc1f26
Tim <tim@memryx.com>
2026-03-03 17:52:48 -0500

    pymodule updated

--- pymodule/bindings.cpp
+++ pymodule/bindings.cpp
@@ -2,18 +2,22 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>  // for std::string
 
+#include <map>
+
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
 #include <numpy/ndarrayobject.h>
 #include <numpy/ndarraytypes.h>
 
-#include "memx/accl/MxAccl.h"
+#include <memx/accl/MxAccl.h>
+
 #include "memx/prepost/MxPrepost.h"
 
 #include <opencv2/opencv.hpp>
 
 namespace py = pybind11;
 using namespace MX::Runtime;
+using namespace MX::Prepost;
 
 cv::Mat numpy_to_mat(const py::array& array) {
     py::array arr = py::array::ensure(array, py::array::c_style);
@@ -39,6 +43,18 @@ cv::Mat numpy_to_mat(const py::array& array) {
 
     // zero-copy conversion
     return cv::Mat(rows, cols, type, info.ptr);
+}
+
+
+// python dict to std::map<int, std::vector<std::string>> (for override_layer_mapping)
+std::map<int, std::vector<std::string>> dict_to_layer_mapping(const py::dict& dict) {
+    std::map<int, std::vector<std::string>> mapping;
+    for (auto item : dict) {
+        int key = item.first.cast<int>();
+        std::vector<std::string> value = item.second.cast<std::vector<std::string>>();
+        mapping[key] = value;
+    }
+    return mapping;
 }
 
 py::array mat_to_numpy(const cv::Mat& mat) {
@@ -79,10 +95,12 @@ class BindMxPrepost {
                   float conf,
                   float iou,
                   std::string classmap_path,
+                  std::vector<std::string> custom_class_labels,
                   std::vector<int> valid_classes,
                   bool fast_sigmoid,
                   bool class_agnostic,
-                  int model_id) {
+                  int model_id,
+                  const py::dict& override_layer_mapping) {
 
         YoloUserConfig config;
         config.conf = conf;
@@ -90,6 +108,7 @@ class BindMxPrepost {
         config.class_agnostic = class_agnostic;
         config.fast_sigmoid = fast_sigmoid;
         config.model_id = model_id;
+        config.override_layer_mapping = dict_to_layer_mapping(override_layer_mapping);
 
         // convert valid_classes vector to unordered_set
         config.valid_classes =
@@ -97,12 +116,13 @@ class BindMxPrepost {
                                         std::make_move_iterator(valid_classes.end()));
 
         config.classmap_path = std::move(classmap_path);
+        config.custom_class_labels = std::move(custom_class_labels);
 
         // TODO: check pyaccl type first
         // get PyMxAccl ptr from pyaccl
         py::object ptr = pyaccl.attr("get_raw_ptr")();
         uintptr_t addr = ptr.cast<uintptr_t>();
-        MX::Runtime::MxAccl* accl = reinterpret_cast<MX::Runtime::MxAccl*>(addr);
+        MxAccl* accl = reinterpret_cast<MxAccl*>(addr);
 
         // create MxPrepost using factory method
         prepost_ = MxPrepost::create(accl, task, config);
@@ -135,7 +155,7 @@ class BindMxPrepost {
         return mat_to_numpy(reshaped);
     }
 
-    MX::Runtime::Result postprocess(const std::vector<py::array>& ofmaps) {
+    MX::Prepost::Result postprocess(const std::vector<py::array>& ofmaps) {
 
         // init ofmap ptrs
         if (ofmap_ptrs_.empty()) {
@@ -159,7 +179,7 @@ class BindMxPrepost {
     }
 
     // NEW overload: postprocess(ofmaps, original_image)
-    MX::Runtime::Result postprocess(const std::vector<py::array>& ofmaps,
+    MX::Prepost::Result postprocess(const std::vector<py::array>& ofmaps,
                                     const py::array& original_image) {
 
         // init ofmap ptrs
@@ -178,13 +198,13 @@ class BindMxPrepost {
         cv::Mat img = numpy_to_mat(original_image);
 
         // call postprocess
-        MX::Runtime::Result result;
+        MX::Prepost::Result result;
         prepost_->postprocess(ofmap_ptrs_, result, img);  // <-- requires C++ overload
         return result;
     }
 
     // NEW overload: postprocess(ofmaps, ori_w, ori_h)
-    MX::Runtime::Result postprocess(const std::vector<py::array>& ofmaps, int ori_w, int ori_h) {
+    MX::Prepost::Result postprocess(const std::vector<py::array>& ofmaps, int ori_w, int ori_h) {
 
         // init ofmap ptrs
         if (ofmap_ptrs_.empty()) {
@@ -199,11 +219,11 @@ class BindMxPrepost {
         }
 
         // call postprocess
-        MX::Runtime::Result result;
+        MX::Prepost::Result result;
         prepost_->postprocess(ofmap_ptrs_, result, ori_w, ori_h);  // <-- requires C++ overload
         return result;
     }
-    py::array draw(py::array& arr, const MX::Runtime::Result& result) {
+    py::array draw(py::array& arr, const MX::Prepost::Result& result) {
         // convert numpy to cv::Mat
         cv::Mat img = numpy_to_mat(arr);
 
@@ -230,19 +250,19 @@ PYBIND11_MODULE(mxprepost, m) {
     numpy_import_array_wrapper();
 
     // Register C++ exception → Python exception
-    py::register_exception<MX::Runtime::UnsupportedTaskError>(m, "UnsupportedTaskError");
+    py::register_exception<MX::Prepost::UnsupportedTaskError>(m, "UnsupportedTaskError");
 
-    py::register_exception<MX::Runtime::MxError>(m, "MxError");
+    py::register_exception<MX::Prepost::MxError>(m, "MxError");
 
     // Result class
-    py::class_<MX::Runtime::Result>(m, "Result")
+    py::class_<MX::Prepost::Result>(m, "Result")
             .def(py::init<>())
             .def_readwrite("boxes", &Result::boxes)
             .def_readwrite("masks", &Result::masks)
             .def_readwrite("keypoints", &Result::keypoints);
 
     // Box class
-    py::class_<MX::Runtime::BBox>(m, "Box")
+    py::class_<MX::Prepost::BBox>(m, "Box")
             .def(py::init<>())
             .def_readwrite("xywh", &BBox::xywh)
             .def_readwrite("xyxy", &BBox::xyxy)
@@ -253,7 +273,7 @@ PYBIND11_MODULE(mxprepost, m) {
     // -----------------------
     // Basic types
     // -----------------------
-    py::class_<MX::Runtime::Point2f>(m, "Point2f")
+    py::class_<MX::Prepost::Point2f>(m, "Point2f")
             // .def(py::init<>())
             .def_readwrite("x", &Point2f::x)
             .def_readwrite("y", &Point2f::y);
@@ -261,15 +281,16 @@ PYBIND11_MODULE(mxprepost, m) {
     // -----------------------
     // Mask
     // -----------------------
-    py::class_<MX::Runtime::Mask>(m, "Mask")
+    py::class_<MX::Prepost::Mask>(m, "Mask")
             // .def(py::init<>())
             .def_readwrite("xys", &Mask::xys)  // list[Point2f]
-            .def_readwrite("cls_id", &Mask::cls_id);
+            .def_readwrite("cls_id", &Mask::cls_id)
+            .def_readwrite("cls_name", &Mask::cls_name);
 
     // -----------------------
     // Keypoint
     // -----------------------
-    py::class_<MX::Runtime::Keypoint>(m, "Keypoint")
+    py::class_<MX::Prepost::Keypoint>(m, "Keypoint")
             // .def(py::init<>())
             // .def(py::init<Point2f, float>(), py::arg("xy"), py::arg("conf"))
             // .def(py::init<float, float, float>(), py::arg("x"), py::arg("y"), py::arg("conf"))
@@ -283,20 +304,24 @@ PYBIND11_MODULE(mxprepost, m) {
                           float,
                           float,
                           std::string,
+                          std::vector<std::string>,
                           std::vector<int>,
                           bool,
                           bool,
-                          int>(),
+                          int,
+                          const py::dict&>(),
                  py::arg("accl"),
                  py::arg("task"),
                  py::arg("conf") = 0.3,
                  py::arg("iou") = 0.4,
                  py::arg("classmap_path") = "",
+                 py::arg("custom_class_labels") = py::list(),
                  py::arg("valid_classes") = py::list(),
                  py::arg("class_agnostic") = false,
                  py::arg("fast_sigmoid") = false,
                  py::arg("model_id") = 0,
-                 R"doc(
+                 py::arg("override_layer_mapping") = py::dict(),
+                 R"pbdoc(
 Create Prepost.
 
 Args:
@@ -305,11 +330,13 @@ Args:
   conf (float): Confidence score. [Default is 0.3]
   iou (float): Intersection over Union (IoU) threshold. [Default is 0.4]
   classmap_path (str): The path for a file containing classes separated in each line. [Default using COCO Classes]
+  custom_class_labels (list of str): List of custom class labels, provided as list instead of file path.
   valid_classes (list of ints): The classes to consider. [Default is all classes]
   fast_sigmoid (bool): Use fast sigmoid if True. [Default is False]
   class_agnostic (bool): Use class-agnostic NMS if True. [Default is False]
   model_id (int): The ID of the model to be used from MemryX accl. [Default is 0]
-)doc")
+  override_layer_mapping (dict): A dictionary mapping stride to list of layer names for coord, conf, mask_coef, keypt ports (if applicable), in case the automatic layer mapping based on output shapes fails. The keys are stride values (e.g., 8, 16, 32) and the values are lists of layer names corresponding to that stride. For example: {16: ["coord_layer_name", "conf_layer_name"], 32: ["coord_layer_name", "conf_layer_name"]}. Use stride=0 for global fmaps such as mask_proto for segmentation. [Default is empty dict, which means auto-mapping will be used]
+)pbdoc")
             .def("draw", &BindMxPrepost::draw)
             .def("preprocess", &BindMxPrepost::preprocess)
             // postprocess overloads
